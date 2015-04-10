092229627695a41982c689142893f6385a531388
Shana Hu <shanawho@gmail.com>
2015-04-09 17:05:18 -0700

    deleted some files that are unnecessary for bridge

--- app/src/main/java/com/example/shana/cherry/app/selectOption.java
+++ app/src/main/java/com/example/shana/cherry/app/selectOption.java
@@ -14,7 +14,6 @@ import android.widget.LinearLayout;
 import android.util.Log;
 import java.util.*;
 
-//import com.philips.lighting.data.AccessPointListAdapter;
 //import com.philips.lighting.data.HueSharedPreferences;
 import com.example.shana.cherry.R;
 
